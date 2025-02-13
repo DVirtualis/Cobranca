51aa2ed47d99ff44623d1b13c7cea4c390675ac6
Danilo Lacerda <daanlacerda@hotmail.com>
2025-02-13 13:28:27 -0300

    substituir seleção de operadora por botões customizados e melhorar exibição das opções

--- cobranca.py
+++ cobranca.py
@@ -238,27 +238,26 @@ def page_cobranca():
         col1, col2, col3 = st.columns([2, 2, 3])
 
         with col1:
-            # Exibir opções com imagem dentro do SelectBox
+            # Seleção de Máquina
             maquina = st.selectbox(
                 "**Máquina**",
                 options=list(MAQUINAS.keys()),
-                format_func=lambda x: f"{x}  🏦" if x in LOGOS_OPERADORAS else x,
                 key="maquina_select"
             )
         
         with col2:
-            # Exibir Operadora com imagem dentro do SelectBox
-            def format_operadora(op):
-                logo_html = f'<img src="{LOGOS_OPERADORAS[op]}" width="25">' if op in LOGOS_OPERADORAS else ""
-                return f"{logo_html} {op}"
+            # Usando botões customizados para operadora
+            operadoras = list(MAQUINAS[maquina].keys())
+            operadora_buttons = [f'<button style="display:block; padding:10px; text-align:center; background-color:#f1f1f1; margin-bottom:5px; border-radius:5px">{f"<img src={LOGOS_OPERADORAS[op]} width=25> {op}"}</button>' for op in operadoras]
             
-            tipo_parcelamento = st.selectbox(
+            # Renderizar as opções de operadora como botão
+            tipo_parcelamento = st.radio(
                 "**Operadora**", 
-                options=list(MAQUINAS[maquina].keys()),
-                format_func=format_operadora,
+                options=operadoras,
+                index=0, 
                 key="operadora_select"
             )
-
+        
         with col3:
             num_parcelas = st.selectbox(
                 "**Forma de Pagamento**",
